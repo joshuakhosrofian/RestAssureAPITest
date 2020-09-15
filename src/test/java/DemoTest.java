0f303aa336b3992012c39f47ef30b6a90a1d352a
joshuakhosrofian <jkhosrofian@alphaleaf.tech>
2020-09-14 21:56:11 -0400

    created demo test cases for checking out DSL syntax

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,30 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+    @Test
+    public void practice_test1() {
+        String url = "https://reqres.in/api/users/2";
+
+        RestAssured.given()
+                .get(url)
+                .andReturn()
+                .getBody()
+                .prettyPrint();
+    }
+
+    @Test
+    public void practice_test2() {
+        String url = "https://swapi.dev/api/people/1";
+
+        Response response = RestAssured.given().
+                get(url).
+                andReturn();
+
+        response.getBody().prettyPrint();
+    }
+
+}
